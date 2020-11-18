030db1bd36c1ea574973bad3db3d2e5c1a5c4de9
Fiterau Andreea <andreeafiterau@yahoo.co.uk>
2020-11-17 19:28:10 -0800

    modified the timesheetView model

--- TSM-API-backend/Dtos/TimesheetViewDto.cs
+++ TSM-API-backend/Dtos/TimesheetViewDto.cs
@@ -10,6 +10,8 @@
 //list of namespaces used in TimesheetViewDto class
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using WebApi.Entities;
 using WebApi.Helpers;
 //list of namespaces
 
@@ -28,36 +30,28 @@ namespace WebApi.Dtos
      ******************************************************/
     public class TimesheetViewDto
     {
+        public int IdTimesheet { get; set; } //!< The Timesheet id
+
         [JsonConverter(typeof(JsonDateConverter))]
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; }  //!< The date of the timesheet
 
-        //[JsonConverter(typeof(JsonDateConverter))]
-        //public DateTime StartTime { get; set; }
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime StartTime { get; set; }  //!< The time a user started working of the specified date
 
-        //[JsonConverter(typeof(JsonDateConverter))]
-        //public DateTime EndTime { get; set; }
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime EndTime { get; set; }  //!< The time a user stopped working of the specified date
 
-        //public TimeSpan BreakTime { get; set; }
+        public TimeSpan BreakTime { get; set; }   //!< The time span of the break in the work day
 
-        //public string Location { get; set; }
+        public string Location { get; set; }  //!< The location where the user is employed
 
-        public string Project { get; set; }
+        public int IdLocation { get; set; } //!< The location id
 
-        public TimeSpan WorkedHours { get; set; }
+        public int IdUser { get; set; }  //!< The user's id
 
-        public string Comments { get; set; }
+        public string Username { get; set; }  //!< The username
 
-        public int IdUser { get; set; }
-
-        public string Username { get; set; }
-
-        public int IdTeam { get; set; }
-
-        public int IdManager { get; set; }
-
-        public int IdProject { get; set; }
-
-        //public int IdTimesheetView { get; set; }
+        public List<TimesheetActivityView> TimesheetActivities { get; set; } //!<List of TimesheetActivities
 
     }//CLASS TimesheetViewDto
 
--- TSM-API-backend/Entities/TimesheetView.cs
+++ TSM-API-backend/Entities/TimesheetView.cs
@@ -10,6 +10,7 @@
 //namespaces used in TimesheetView class
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using WebApi.Helpers;
 //list of namespaces
 
@@ -27,37 +28,29 @@ namespace WebApi.Entities
      ******************************************************/
     public class TimesheetView
     {
+        public int IdTimesheet { get; set; } //!< The Timesheet id
+
         [JsonConverter(typeof(JsonDateConverter))]
         public DateTime Date { get; set; }  //!< The date of the timesheet
 
-        //[JsonConverter(typeof(JsonDateConverter))]
-        //public DateTime StartTime { get; set; }  //!< The time a user started working of the specified date
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime StartTime { get; set; }  //!< The time a user started working of the specified date
 
-        //[JsonConverter(typeof(JsonDateConverter))]
-        //public DateTime EndTime { get; set; }  //!< The time a user stopped working of the specified date
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime EndTime { get; set; }  //!< The time a user stopped working of the specified date
 
-        //public TimeSpan BreakTime { get; set; }   //!< The time span of the break in the work day
+        public TimeSpan BreakTime { get; set; }   //!< The time span of the break in the work day
 
-        //public string Location { get; set; }  //!< The location the user is employed
+        public string Location { get; set; }  //!< The location where the user is employed
 
-        public string Project { get; set; }   //!< The project the user is working on
-
-        public TimeSpan WorkedHours { get; set; }   //!< The time span a user worked on the specified project in the specified date
-
-        public string Comments { get; set; }   //!< The comments a user had on the work from specified date,specified project
+        public int IdLocation { get; set; } //!< The location id
 
         public int IdUser { get; set; }  //!< The user's id
 
         public string Username { get; set; }  //!< The username
 
-        public int IdTeam { get; set; } //!< The id of the user's team
+        public List<TimesheetActivityView> TimesheetActivities { get; set; } //!<List of TimesheetActivities
 
-        public int IdManager { get; set; } //!< The id of the user's manager
-
-        public int IdProject { get; set; } //!< The id of the specified project
-
-        [System.ComponentModel.DataAnnotations.Key]
-        public int IdTimesheetView { get; set; }
 
     }//CLASS TimesheetView
 
