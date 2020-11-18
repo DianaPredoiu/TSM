228c7081ea3645bf25d17018a50b83a5516c16ab
Fiterau Andreea <andreeafiterau@yahoo.co.uk>
2020-11-17 19:31:04 -0800

    added timesheet activity view

---
+++ TSM-API-backend/Entities/TimesheetActivityView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApi.Entities
+{
+    public class TimesheetActivityView
+    {       
+        public int IdTimesheetActivity { get; set; }  //!< The id of the timesheet activity
+      
+        public int IdTimesheet { get; set; }  //!< The timesheet a user worked on (id of the timesheet)
+       
+        public int IdProject { get; set; }  //!< The project a user worked on (id of the project)
+
+        public string ProjectName { get; set; } //!< The project's name
+
+        public string Comments { get; set; }  //!< The comments a user had on the work from specified date,specified project
+
+        public TimeSpan WorkedHours { get; set; } //!< The number of hours a user spent to work on a project on a specified date
+    }
+}
